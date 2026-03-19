d4df74c82f3b1fedf1d4659fe3969c9541cf4d78
GagandeepDhami <gagandeep_k_dhami@progressive.com>
2026-03-19 20:57:45 +0000

    lab: counter, i decided to do a diffrent lab then i did yesterday and im still working on it, i did the feature scaffold, counter page, addign a routing link,a dn was working on making the counter work>

---
+++ src/app/areas/counter/counter-landing/counter.routes.ts
@@ -0,0 +1,46 @@
+import { Routes } from '@angular/router';
+import { Home } from './internal/home';
+import { HomePage } from './internal/pages/home';
+import { CounterPage } from './internal/pages/counter';
+
+export const counterFeatureRoutes: Routes = [
+  {
+    path: '',
+    providers: [],
+    component: Home,
+    children: [
+      {
+        path: 'counter',
+        component: CounterPage,
+      },
+    ],
+  },
+];
+
+
+export const signalsdemosFeatureRoutes: Routes = [
+  {
+    path: '',
+    providers: [counterStore],
+
+    // scoped (available) to anything in this feature,
+    // but not outside of this.
+    // it does not remove (destroy ) when you leave.
+    component: Home,
+
+    children: [
+      {
+        path: '',
+        component: HomePage,
+      },
+      {
+        path: 'counter',
+        component: CounterPage,
+      },
+      {
+        path: 'prefs',
+        component: PrefsPage,
+      },
+    ],
+  },
+];
