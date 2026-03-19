b3bcfccc28a2d9155ed32a726fb27ad9d831b119
Jake Cousineau <cousi2344@gmail.com>
2026-03-19 14:28:05 -0400

    very basic UI for books list

--- src/app/areas/books/books-landing/internal/pages/list.ts
+++ src/app/areas/books/books-landing/internal/pages/list.ts
@@ -3,12 +3,23 @@ import { Component } from '@angular/core';
 import { PageLayout } from '@ht/shared/ui-common/layouts/page';
 import { BooksApiItemModel } from '../types';
 import { JsonPipe } from '@angular/common';
+import { BasicCard } from '@ht/shared/ui-common/cards/basic-card';
 
 @Component({
   selector: 'app-books-pages-list',
-  imports: [PageLayout, JsonPipe],
+  imports: [PageLayout, JsonPipe, BasicCard],
   template: `<app-ui-page title="List">
-    <pre>{{ booksResource.value() | json }}</pre>
+    <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-4">
+      @for (book of booksResource.value(); track book.id) {
+        <app-ui-card-basic [title]="book.title">
+          <div class="flex gap-1">
+            <span>{{ book.title }}</span>
+            <span>{{ book.author }}</span>
+            <span>{{ book.year }}</span>
+          </div>
+        </app-ui-card-basic>
+      }
+    </div>
   </app-ui-page>`,
   styles: ``,
 })
